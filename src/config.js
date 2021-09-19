8c409d588780247e95c83a00a7c29bba43003c15
Muhammad Sharjeel <79161846+sharjeel125@users.noreply.github.com>
2021-09-19 16:16:27 -0700

    first commit

--- src/config.js
+++ src/config.js
@@ -10,78 +10,51 @@ module.exports = {
         }
     },
     social: {
-        linkedin: 'ariful-alam',
-        twitter: 'arif_swozon',
-        facebook: '',
+        linkedin: 'https://www.linkedin.com/in/muhammad-sharjeel-7a540a1b7/',
+        
+        facebook: 'https://www.facebook.com/muhammad.abdulsharjeel/',
         dribbble: '',
         behance: '',
         medium: '',
         devto: '',
-        website: 'https://arifszn.github.io',
+        website: '',
         phone: '',
         email: ''
     },
     skills: [
-        'PHP',
-        'Laravel',
+        
         'JavaScript',
         'React.js',
-        'Vue.js',
         'Node.js',
         'Jquery',
-        'MySQL',
+        'NoSQL',
         'Git',
         'CSS',
-        'Antd',
         'Tailwind',
         'Bootstrap',
     ],
-    experiences: [
-        { 
-            company: 'Monstarlab Bangladesh',
-            position: 'Software Engineer',
-            from: 'September 2021',
-            to: 'Present'
-        },
-        { 
-            company: 'Orangetoolz - My Offer 360 Degree',
-            position: 'Jr. Full Stack Engineer',
-            from: 'July 2019',
-            to: 'August 2021'
-        },
-        { 
-            company: 'Techvillage',
-            position: 'Jr. Software Engineer',
-            from: 'January 2019',
-            to: ' June 2019'
-        }
-    ],
+    ,
     education: [
         { 
-            institution: 'American International University-Bangladesh',
+            institution: 'University of karachi',
             degree: 'Bachelor of Science',
-            from: '2015',
-            to: '2019'
+            from: '2019',
+            to: '2022'
         },
         { 
-            institution: 'Cantonment College, Jessore',
+            institution: 'P.E.C.H.S College, karachi',
             degree: 'Higher Secondary Certificate (HSC)',
-            from: '2012',
-            to: '2014',
+            from: '2016',
+            to: '2018',
         },
         { 
-            institution: 'Chowgacha Shahadat Pilot High School',
+            institution: 'ETN Nasra School',
             degree: 'Secondary School Certificate (SSC)',
-            from: '2007',
-            to: '2012'
+            from: '2004',
+            to: '2016'
         }
     ],
-    blog: {
-        // Display blog posts from your medium or dev.to account. (Optional)
-        source: 'dev.to', // medium | dev.to
-        username: 'arifszn',
-        limit: 2 // How many posts to display. Max is 10.
-    },
+    ,
     googleAnalytics: {
         // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
         id: 'G-WLLB5E14M6' // Please remove this and use your own tag id
