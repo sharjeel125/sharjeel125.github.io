d136b2093eb26022412e2e84d33fc60937b46cfe
Muhammad Sharjeel <79161846+sharjeel125@users.noreply.github.com>
2021-09-19 18:19:14 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -36,44 +36,25 @@ module.exports = {
         'Tailwind',
         'Bootstrap',
     ],
-    experiences: [
-        { 
-            company: 'Monstarlab Bangladesh',
-            position: 'Software Engineer',
-            from: 'September 2021',
-            to: 'Present'
-        },
-        { 
-            company: 'Orangetoolz - My Offer 360 Degree',
-            position: 'Jr. Full Stack Engineer',
-            from: 'July 2019',
-            to: 'August 2021'
-        },
-        { 
-            company: 'Techvillage',
-            position: 'Jr. Software Engineer',
-            from: 'January 2019',
-            to: ' June 2019'
-        }
-    ],
+    
     education: [
-        { 
-            institution: 'American International University-Bangladesh',
+         { 
+            institution: 'University of karachi',
             degree: 'Bachelor of Science',
-            from: '2015',
-            to: '2019'
+            from: '2019',
+            to: '2022'
         },
         { 
-            institution: 'Cantonment College, Jessore',
+            institution: 'P.E.C.H.S College, karachi',
             degree: 'Higher Secondary Certificate (HSC)',
-            from: '2012',
-            to: '2014',
+            from: '2016',
+            to: '2018',
         },
         { 
-            institution: 'Chowgacha Shahadat Pilot High School',
+            institution: 'ETN Nasra School',
             degree: 'Secondary School Certificate (SSC)',
-            from: '2007',
-            to: '2012'
+            from: '2004',
+            to: '2016'
         }
     ],
     blog: {
