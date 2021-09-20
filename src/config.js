93510f0858ec6cda78827b1149e1558ce0de3760
Muhammad Sharjeel <79161846+sharjeel125@users.noreply.github.com>
2021-09-19 18:15:37 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -10,52 +10,82 @@ module.exports = {
         }
     },
     social: {
-        linkedin: 'muhammad-sharjeel-7a540a1b7',
-        
-        facebook: 'muhammad.abdulsharjeel',
+        linkedin: 'ariful-alam',
+        twitter: 'arif_swozon',
+        facebook: '',
         dribbble: '',
         behance: '',
         medium: '',
         devto: '',
-        website: '',
+        website: 'https://arifszn.github.io',
         phone: '',
         email: ''
     },
     skills: [
-        
+        'PHP',
+        'Laravel',
         'JavaScript',
         'React.js',
+        'Vue.js',
         'Node.js',
-        
-        'NoSQL',
+        'Jquery',
+        'MySQL',
         'Git',
         'CSS',
+        'Antd',
         'Tailwind',
         'Bootstrap',
     ],
-    
-    education: [
+    experiences: [
         { 
-            institution: 'University of karachi',
-            degree: 'Bachelor of Science',
-            from: '2019',
-            to: '2022'
+            company: 'Monstarlab Bangladesh',
+            position: 'Software Engineer',
+            from: 'September 2021',
+            to: 'Present'
         },
         { 
-            institution: 'P.E.C.H.S College, karachi',
-            degree: 'Higher Secondary Certificate (HSC)',
-            from: '2016',
-            to: '2018',
+            company: 'Orangetoolz - My Offer 360 Degree',
+            position: 'Jr. Full Stack Engineer',
+            from: 'July 2019',
+            to: 'August 2021'
         },
         { 
-            institution: 'ETN Nasra School',
-            degree: 'Secondary School Certificate (SSC)',
-            from: '2004',
-            to: '2016'
+            company: 'Techvillage',
+            position: 'Jr. Software Engineer',
+            from: 'January 2019',
+            to: ' June 2019'
         }
     ],
-    
-  
+    education: [
+        { 
+            institution: 'American International University-Bangladesh',
+            degree: 'Bachelor of Science',
+            from: '2015',
+            to: '2019'
+        },
+        { 
+            institution: 'Cantonment College, Jessore',
+            degree: 'Higher Secondary Certificate (HSC)',
+            from: '2012',
+            to: '2014',
+        },
+        { 
+            institution: 'Chowgacha Shahadat Pilot High School',
+            degree: 'Secondary School Certificate (SSC)',
+            from: '2007',
+            to: '2012'
+        }
+    ],
+    blog: {
+        // Display blog posts from your medium or dev.to account. (Optional)
+        source: 'dev.to', // medium | dev.to
+        username: 'arifszn',
+        limit: 2 // How many posts to display. Max is 10.
+    },
+    googleAnalytics: {
+        // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+        id: 'G-WLLB5E14M6' // Please remove this and use your own tag id
+    },
     themeConfig: {
         default: 'light',
 
